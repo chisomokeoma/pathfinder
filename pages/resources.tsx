e844ded285d46fcf0438444e35f990310f2a2ea1
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2024-04-09 08:25:07 +0100

    added my images

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-function Resources() {
-  return (
-    <div>Resources</div>
-  )
-}
+// function Resources() {
+//   return (
+//     <div>Resources</div>
+//   )
+// }
 
-export default Resources
+// export default Resources
