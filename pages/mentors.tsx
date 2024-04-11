2e1992c1bc89ab8fa8c8bbe2389fccce03d7b37a
soniachinwe <soniachinwe176@gmail.com>
2024-04-11 10:58:21 +0100

    mentors page

--- pages/mentors.tsx
+++ pages/mentors.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 
 function Mentors() {
   return (
-    <main className='box-border flex'>
-      <div className='flex justify-center flex-col w-[1080px] h-[1305px] bg-[#ffff]'>
-        <div className='w-[1086px] h-[261px]' ></div>
-        <div className='w-[1086px] h-[261px]'></div>
+    <main className='box-border'>
+      <div className='flex justify-center w-1080px h-1305px bg-#ffff'>
+        <div className='w-1086px h-261px bg-#F87171 flex flex-col'></div>
+        <div className='w-1086px h-261px flex flex-col'></div>
+        <div className="w-1086px h-261px flex flex-col"></div>
       </div>
 
 
