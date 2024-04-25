42f126ed086a378eda621c959e52b872064c57b0
soniachinwe <soniachinwe176@gmail.com>
2024-04-25 11:49:33 +0100

    mentors page

---
+++ pages/chat.tsx
@@ -0,0 +1,92 @@
+
+
+function chat() {
+  return (
+    <main className="flex flex-col gap-[30px] h-full w-[1650px]">
+        <div className="w-[395px] h-[72px] font-bold text-[40px] text-[#161439]">Chats</div>
+        <div className="flex gap-[10px] flex-shrink">
+            <div className="flex flex-col gap-[10px]">
+                <div className="w-[641px] h-[117px] shadow-inner bg-[#f6f6f6] flex">
+                    <div className="w-[100px] h-[100px] flex mt-[5px]"><img className="w-[100px] h-[100px]" src="amina.svg/" alt="" /></div>
+                    <div className="flex flex-col w-[413px] h-[51px] ml-[60px] gap-[30px] mt-[25px]">
+                        <div className="w-[117px] h-[27px] font-semibold text-[14px] text-[#161439]">Amina Yusuf</div>
+                        <div className="w-[296px] h-[24px] text-[10px]">Is it owkay to proceed using a voice ..</div>
+                    </div>
+                    <div className="w-[103.1px] h-[35px] text-[#161439] mt-[85px] text-[10px] ml-[10px]">12:41pm</div>
+                </div>
+                <div className="w-[641px] h-[117px] shadow-inner bg-[rgb(246,246,246)]/50 flex">
+                <div className="w-[118px] h-[114px] flex mt-[5px]"><img className="w-[100px] h-[100px]" src="emeka2.svg/" alt="" /></div>
+                    <div className="flex flex-col w-[413px] h-[51px] ml-[50px] gap-[30px] mt-[25px]">
+                        <div className="w-[117px] h-[27px] font-semibold text-[14px] text-[#161439]">Raphael Emeka</div>
+                        <div className="w-[296px] h-[24px] text-[10px]">I decided to message you because ..</div>
+                    </div>
+                    <div className="w-[103.1px] h-[35px] text-[#161439] mt-[85px] text-[10px] ml-[10px]">11:39am</div>
+                </div>
+                <div className="w-[641px] h-[117px] shadow-inner bg-[rgb(246,246,246)]/50 flex">
+                <div className="w-[134px] h-[131px] flex"><img className="w-[118.75px] h-[118.75px]" src="tina.svg/" alt=""/></div>
+                    <div className="flex flex-col w-[413px] h-[51px] ml-[35px] gap-[30px] mt-[25px]">
+                        <div className="w-[117px] h-[27px] font-semibold text-[14px] text-[#161439]">Tina Mba</div>
+                        <div className="w-[296px] h-[24px] text-[10px]">I decided to message you because ..</div>
+                    </div>
+                    <div className="w-[103.1px] h-[35px] text-[#161439] mt-[85px] text-[10px] ml-[10px]">11:48pm</div>
+                </div>
+                <div className="w-[641px] h-[117px] shadow-inner bg-[rgb(246,246,246)]/50 flex">
+                <div className="w-[150px] h-[150px] flex"><img className="w-[118.75px] h-[118.75px]" src="segun.svg/" alt="" /></div>
+                    <div className="flex flex-col w-[413px] h-[51px] ml-[25px] gap-[30px] mt-[25px]">
+                        <div className="w-[117px] h-[27px] font-semibold text-[14px] text-[#161439]">Segun Idris</div>
+                        <div className="w-[296px] h-[24px] text-[10px]">I decided to message you because ..</div>
+                    </div>
+                    <div className="w-[103.1px] h-[35px] text-[#161439] mt-[85px] text-[10px] ml-[10px]">11:50am</div>
+                </div>
+                <div className="w-[641px] h-[117px] shadow-inner bg-[rgb(246,246,246)]/50 flex">
+                <div className="w-[118px] h-[114px] flex mt-[5px] ml-[5px]"><img className="w-[95px] h-[95px]" src="emeka2.svg/" alt="" /></div>
+                    <div className="flex flex-col w-[413px] h-[51px] ml-[50px] gap-[30px] mt-[25px]">
+                        <div className="w-[117px] h-[27px] font-semibold text-[14px] text-[#161439]">Raphael Emeka</div>
+                        <div className="w-[296px] h-[24px] text-[10px]">I decided to message you because ..</div>
+                    </div>
+                    <div className="w-[103.1px] h-[35px] text-[#161439] mt-[85px] text-[10px] ml-[10px]">11:40am</div>
+                </div>
+                <div className="w-[641px] h-[117px] shadow-inner bg-[rgb(246,246,246)]/50 flex">
+                <div className="w-[100px] h-[100px] flex"><img src="tina.svg/" alt="" /></div>
+                    <div className="flex flex-col w-[413px] h-[51px] ml-[70px] gap-[30px] mt-[25px]">
+                        <div className="w-[117px] h-[27px] font-semibold text-[14px] text-[#161439]">Tina Mba</div>
+                        <div className="w-[296px] h-[24px] text-[10px]">I decided to message you because ..</div>
+                    </div>
+                    <div className="w-[103.1px] h-[35px] text-[#161439] mt-[85px] text-[10px] ml-[10px]">10:33am</div>
+                </div>
+                <div className="w-[641px] h-[117px] shadow-inner bg-[rgb(246,246,246)]/50 flex">
+                <div className="w-[100px] h-[100px] flex"><img src="segun.svg/" alt="" /></div>
+                    <div className="flex flex-col w-[413px] h-[51px] ml-[70px] gap-[30px] mt-[25px]">
+                        <div className="w-[117px] h-[27px] font-semibold text-[14px] text-[#161439]">Segun Idris</div>
+                        <div className="w-[296px] h-[24px] text-[10px]">I decided to message you because ..</div>
+                    </div>
+                    <div className="w-[103.1px] h-[35px] text-[#161439] mt-[85px] text-[10px] ml-[10px]">12:41pm</div>
+                </div>
+                <button className="w-[40px] h-[40px] mx-auto mt-[5px]"><img className="w-[40px] h-[40px]" src="down.svg/" alt="" /></button>
+
+            </div>
+            <div className="w-[983px] h-[1128px] shadow-inner border-2 flex flex-col flex-shrink">
+                <div className="w-[983px] h-[117px] border-2 rounded-[5px] flex">
+                    <div className="w-[100px] h-[100px] ml-[20px] mt-[5px]"><img className="w-[100px] h-[100px]" src="amina.svg/" alt="" /></div>
+                    <div className="w-[119px] h-[27px] text-[14px] font-bold mt-[50px] ml-[30px]">Amina Yusuf</div>
+                    <div className="flex gap-[45px] ml-[550px] mt-[45px] w-[100px] h-[70px]">
+                        <div className="w-[50px] h-[35px] mb-[10px]"><img src="phone.svg/" alt="voice call" /></div>
+                        <div className="w-[50px] h-[35px] mt-[5px]"><img src="video.svg/" alt="video call" /></div> 
+                    </div>      
+                </div>
+                <div className="w-[87px] h-[20px] text-[#1E1E1E] text-[10px] mx-auto flex text-justify mt-[491.5px]">Today</div>
+                <div className="w-[342px] h-[82px] flex justify-start bg-[#ECE8FF] ml-[10px] mt-[20px] rounded-[5px] items-center"><div className=" flex w-[294px] h-[48px] text-justify font-bold text-[12px] text-[#4B0082] justify-center ml-[20px]">Good afternoon Ngozi. Thank you for accepting my request</div></div>
+                <div className="w-[306px] h-[82px] flex justify-end bg-[#4B0082] rounded-[5px] ml-[658px] mt-[10px]"><div className="flex text-justify w-[208px] h-[24px] text-[#ffff] font-bold text-[12px] mt-[25px]">Nice to meet you Amina</div></div>
+                <div className="w-[342px] h-[82px] flex justify-start bg-[#ECE8FF] ml-[10px] mt-[30px] rounded-[5px] items-center"><div className=" flex w-[294px] h-[48px] text-justify font-bold text-[12px] text-[#4B0082] justify-center ml-[20px]">Good afternoon Ngozi. Thank you for accepting my request</div></div>
+                <div className="w-[983px] h-[84px] border-2 rounded-[5px] shadow-lg mt-[105px] flex">
+                    <div className="w-[927px] h-[24px]"><input className="w-[927px] h-[24px] font-regular text-[12px] mt-[25px] ml-[5px] flex-grow" placeholder="Type a message" type="text" /></div>
+                    <div className="w-[24px] h-[24px] mt-[25px] ml-[2px]"><img src="microphone.svg/" alt="" /></div>
+
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default chat
